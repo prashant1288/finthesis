fc86a6bf0c5f38172e450e1e987bb0484b46827d
HAPPY <HAPPY@Prashant>
2017-07-10 00:53:25 -0400

    prototype

---
+++ src/main/java/com/ft/prototype1/Prototype1Application.java
@@ -0,0 +1,12 @@
+package com.ft.prototype1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prototype1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prototype1Application.class, args);
+	}
+}
